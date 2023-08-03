b65b15fee4d3ee2dbfcee3a3b443e496bb0c69ae
Wesleyhanauer <whanauerbiz@proton.me>
2023-08-03 19:09:07 -0300

    Added a main controller and an "Add income" button

---
+++ src/main/java/com/extrak/mainController.java
@@ -0,0 +1,12 @@
+package com.extrak;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+
+public class mainController {
+    public Button buttonAddNew;
+    public ChoiceBox<?> selector;
+    public void initialize(){
+        buttonAddNew.setText("Add income");
+    }
+}
