2d789fbd0899f85bf3494494142531470c6b8d5a
Wesleyhanauer <whanauerbiz@proton.me>
2023-08-06 22:58:30 -0300

    Added different data tables for income and expenses

--- src/main/java/com/extrak/mainController.java
+++ src/main/java/com/extrak/mainController.java
@@ -1,11 +1,30 @@
 package com.extrak;
 
+import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TableView;
 
 public class mainController {
-    public Button buttonAddNew;
-    public ChoiceBox<?> selector;
+    @FXML
+    private Button buttonAddNew;
+    @FXML
+    private TableView<?> expensesTable;
+    @FXML
+    private TableView<?> incomeTable;
+    @FXML
+    public ChoiceBox<String> selector;
+    public void changeTable(){
+        if(selector.getValue().equals("Expenses")){
+            expensesTable.setVisible(true);
+            incomeTable.setVisible(false);
+        }
+        else{
+            incomeTable.setVisible(true);
+            expensesTable.setVisible(false);
+        }
+    }
+    @FXML
     public void initialize(){
         buttonAddNew.setText("Add income");
     }
